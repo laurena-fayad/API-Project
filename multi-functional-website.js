0b0d66d4cb538a849cdcb57816e92eaa682a34fb
Laurena Fayad <laurenafayad@gmail.com>
2022-02-09 10:30:16 +0200

    added initial html divs

---
+++ multi-functional-website.js
@@ -0,0 +1,6 @@
+let math_api = "http://localhost/api-project/math-equation.php/?operand1=&operand2="
+let palindrome_api = "http://localhost/api-project/palindrome-checker.php/?string="
+let string_cleaner_api = "http://localhost/api-project/string-cleaner.php/?string="
+let password_api = "http://localhost/api-project/password-validator.php/?password="
+
+
---
+++ palindrome-checker.php
@@ -0,0 +1,21 @@
+<?php
+
+//API 2
+$string = $_GET["string"];
+
+$reversed_string = strrev($string);
+
+$result;
+if($string != null){
+    if ($reversed_string == $string){
+        $result = "Palindrome";
+    }
+    else{
+        $result = "Not a palindrome";
+    }
+    
+    $array = ["string"=>$string, "result"=>$result];
+    
+    echo json_encode($array);    
+}
+?>
